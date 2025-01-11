e3074b6bdb62234370f250f06b8cce2cdbd89e15
Anup Saud <anupsaud10@gmail.com>
2025-01-11 07:46:01 -0500

    inital project structure

---
+++ MagicTravel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicTravel
+//
+//  Created by Anup Saud on 2025-01-11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
