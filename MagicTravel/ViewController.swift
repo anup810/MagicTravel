2bcc18713446795e06cb918cd43a428aecc3f477
Anup Saud <anupsaud10@gmail.com>
2025-01-14 18:09:03 -0500

    Implement basic UITableView with dynamic data to display a list of countries while learning about UITableView's data source and delegate methods.

--- MagicTravel/ViewController.swift
+++ MagicTravel/ViewController.swift
@@ -8,12 +8,68 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var countriesTableView: UITableView!
+    let countries = [
+        "Canada",
+        "United States",
+        "United Kingdom",
+        "Australia",
+        "Germany",
+        "France",
+        "Italy",
+        "Spain",
+        "Japan",
+        "India",
+        "Brazil",
+        "Mexico",
+        "South Korea",
+        "China",
+        "Russia",
+        "South Africa",
+        "New Zealand",
+        "Argentina",
+        "Norway",
+        "Sweden"
+    ]
+
+    
+    struct Constants{
+        static let cellIdentifier = "MagicCountryCell"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        countriesTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MagicCountryCell")
+        
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tableViewCell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath)
+        var tableViewDefaultContentConfiguration = tableViewCell.defaultContentConfiguration()
+        tableViewDefaultContentConfiguration.text = countries[indexPath.row]
+        tableViewCell.contentConfiguration = tableViewDefaultContentConfiguration
+      
+        return tableViewCell
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+
+    
+}
