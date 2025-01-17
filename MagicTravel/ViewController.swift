bccc1c589e3050af45f2ad55e1e9fbf0c99e3690
Anup Saud <anupsaud10@gmail.com>
2025-01-16 22:26:55 -0500

    Enhance UITableView to display countries with continents and add navigation to detail view on cell selection.

--- MagicTravel/ViewController.swift
+++ MagicTravel/ViewController.swift
@@ -32,6 +32,29 @@ class ViewController: UIViewController {
         "Sweden"
     ]
 
+    var continentCountry: [String: String] = [
+        "Canada": "North America",
+        "United States": "North America",
+        "United Kingdom": "Europe",
+        "Australia": "Australia",
+        "Germany": "Europe",
+        "France": "Europe",
+        "Italy": "Europe",
+        "Spain": "Europe",
+        "Japan": "Asia",
+        "India": "Asia",
+        "Brazil": "South America",
+        "Mexico": "North America",
+        "South Korea": "Asia",
+        "China": "Asia",
+        "Russia": "Europe/Asia",
+        "South Africa": "Africa",
+        "New Zealand": "Australia",
+        "Argentina": "South America",
+        "Norway": "Europe",
+        "Sweden": "Europe"
+    ]
+
     
     struct Constants{
         static let cellIdentifier = "MagicCountryCell"
@@ -57,7 +80,9 @@ extension ViewController: UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let tableViewCell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath)
         var tableViewDefaultContentConfiguration = tableViewCell.defaultContentConfiguration()
-        tableViewDefaultContentConfiguration.text = countries[indexPath.row]
+        let countryName = countries[indexPath.row]
+        tableViewDefaultContentConfiguration.text = countryName
+        tableViewDefaultContentConfiguration.secondaryText = continentCountry[countryName] ?? ""
         tableViewCell.contentConfiguration = tableViewDefaultContentConfiguration
       
         return tableViewCell
@@ -67,9 +92,15 @@ extension ViewController: UITableViewDataSource{
 }
 
 extension ViewController: UITableViewDelegate{
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
-    }
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("User Tapped on the cell at\(indexPath)")
+        
+        let detailViewController = UIViewController()
+        detailViewController.view.backgroundColor = .gray
+        detailViewController.modalPresentationStyle = .automatic
+//        present(detailViewController, animated: true)
+        navigationController?.pushViewController(detailViewController, animated: true)
+    }
     
 }
